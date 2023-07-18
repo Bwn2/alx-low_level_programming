8eb5b1052ab68e50209c0844043b7d3fe65c965e
Bwn2 <wangaibarnabas@gmail.com>
2023-07-18 02:12:35 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,29 @@
-#include "holberton.h"
-/**
- * main - Print "Holberton" followed by a new line.
- * Description: You are not allowed to include standard libraries.
- * Return: 0
+ 
+
+ /*
+ * File: 0-putchar.c 
+ *
  */
-int main(void)
-{
-	char holberton[] = "Holberton";
-	int i = 0;
 
-	while (holberton[i] != '\0')
-	{
-		_putchar(holberton[i]);
-		i++;
-	}
-	_putchar('\n');
+ #include "main.h" 
 
-	return (0);
-}
+ /** 
+ * main-print putchar followed by a new line
+ * 
+ *Return :always 0
+ */
+  int main(void) 
+ {
+      _putchar('_' );
+      _putchar ('p' );
+      _putchar ('u' );
+      _putchar ('t' );
+      _putchar ('c' );
+      _putchar ('h' );
+      _putchar ('a' );
+      _putchar ('r' );
+      _putchar ('\n');
+      
+       return (0) ;
+} 
+      
