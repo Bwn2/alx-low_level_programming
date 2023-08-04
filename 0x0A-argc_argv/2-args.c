2a4497162073e85c243479c637f58298137e6a4e
Bwn2 <wangaibarnabas@gmail.com>
2023-08-04 16:12:29 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints  arguments
+ * @argc: argument counting
+ * @argv: args
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+int y  = 0;
+do {
+printf("%s\n", argv[y]);
+y++;
+} while (y < argc);
+return (0);
+}
